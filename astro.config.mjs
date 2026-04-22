e6e1db33e8310c0d68ce0fc678598c2db441c4a9
roschreiber <schreiber.robin@icloud.com>
2026-04-22 23:13:55 +0200

    add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
+  experimental: {
     rustCompiler: true,
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Moonhouse",
+      cssVariable: "--font-moonhouse",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Moonhouse.ttf"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Quicksand",
+      cssVariable: "--font-quicksand",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Quicksand-Regular.ttf"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
